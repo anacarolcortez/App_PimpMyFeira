6f77869e94660b668b14be690f3d5e8811c4b3f8
Ana Carol Cortez <56210395+anacarolcortez@users.noreply.github.com>
2020-11-01 19:36:35 -0300

    inclusão de filtro por ingrediente

--- views/Listagem.js
+++ views/Listagem.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import { Text, View, StyleSheet, ScrollView  } from 'react-native'
-import Filtro from '../components/Filtros'
+import React, { useState } from 'react'
+import { Text, TextInput, View, StyleSheet, ScrollView  } from 'react-native'
 import Sinopse from '../components/Sinopse'
 
+import receitas from '../bd/receitas'
+
 export default props => {
+
+    const [receitasSelecionadas, setReceitasSelecionadas] = useState([...receitas])
+    const [search, setSearch] = useState('')
+
+    selecionarReceitas = (txt) => {
+        setSearch(txt.toLowerCase())
+        const res = receitas.filter(receita => receita.nome.includes(search))
+        setReceitasSelecionadas(res)
+    }
+
     return(
         <View style={styles.bgcolor}>
-        <Filtro />
-        <Text style={styles.title}>Livro de Receitas</Text>
-        <Text style={styles.subtitle}>Confira pratos deliciosos que aproveitam partes super nutritivas de alimentos que descartamos</Text>
-        <ScrollView>
-            <Sinopse navigation={props.navigation}/>
-        </ScrollView>
+            <View style={styles.filter}>
+                <TextInput placeholder="Pesquise por ingrediente" style={styles.inputText}
+                onChangeText={(txt) => this.selecionarReceitas(txt)} 
+                value={search}/>
+            </View>
+            <Text style={styles.title}>Livro de Receitas</Text>
+            <Text style={styles.subtitle}>Confira pratos deliciosos que aproveitam partes super nutritivas de alimentos que descartamos</Text>
+            <ScrollView>
+                <Sinopse navigation={props.navigation} filtroReceitas={receitasSelecionadas}/>
+            </ScrollView>
         </View>
     )
 }
@@ -37,4 +52,15 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         fontStyle: 'italic',
     },
+    filter :{
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        padding: 10,
+        backgroundColor: '#ff801a',
+        marginTop: 40,
+    },
+    inputText: {
+        color: '#ffffff',
+        fontSize: 16,
+    }
 })
